a273925e5945f5a4fcb5f3798d78d572cb4c29b1
RevenzMind <155520951+RevenzMind@users.noreply.github.com>
2024-03-19 20:08:23 -0600

    Add @astrojs/vercel dependency and change output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   adapter: vercel(),
 });
