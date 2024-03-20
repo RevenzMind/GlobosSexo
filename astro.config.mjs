6e49bbb198b0bf4ba8a7507a99d883cb0869f163
RevenzMind <155520951+RevenzMind@users.noreply.github.com>
2024-03-19 20:06:34 -0600

    Update astro.config.mjs to use @astrojs/vercel/serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
 export default defineConfig({
-    output: 'hybrid',
-    adapter: node({
-        mode: "standalone"
-      })
+  output: 'server',
+  adapter: vercel(),
 });
-
-
