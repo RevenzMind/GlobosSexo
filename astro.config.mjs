399e51677413b3b5bcdecd9dad48c4661a729084
RevenzMind <155520951+RevenzMind@users.noreply.github.com>
2024-03-19 20:16:40 -0600

    fucking canvas error!??!?!?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+      `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+  ) {
+    process.env.LD_LIBRARY_PATH = `${
+      process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+  }
+
+
 export default defineConfig({
   output: 'hybrid',
   adapter: vercel(),
